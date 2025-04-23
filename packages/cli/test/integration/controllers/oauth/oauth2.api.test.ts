63a325b19c6b36ef3525e8d4a901f5be9c1055f0
rg7services <133136316+rg7services@users.noreply.github.com>
2025-04-23 15:07:38 -0500

    Update oauth2.api.test.ts

--- packages/cli/test/integration/controllers/oauth/oauth2.api.test.ts
+++ packages/cli/test/integration/controllers/oauth/oauth2.api.test.ts
@@ -69,7 +69,7 @@ describe('OAuth2 API', () => {
 		expect(parseQs(authUrl.search.slice(1))).toEqual({
 			access_type: 'offline',
 			client_id: 'client_id',
-			redirect_uri: 'http://localhost:5678/rest/oauth2-credential/callback',
+			redirect_uri: 'https://n8n-dulton-consultans-production.up.railway.app/',
 			response_type: 'code',
 			state,
 			scope: 'openid',
